630d68867f060f9a7d171cb48d2d41505268b27f
s01116196 <33640790+s01116196@users.noreply.github.com>
2017-12-04 02:55:33 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,72 +4,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab008
+namespace IT1050_Midterm
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
-            Console.WriteLine(names[1] + " " + names[4]);
+            int unit = 1;
+            Boolean keepLooping = true;
+            while (keepLooping == true) Console.Write(unit + 1);
 
-
+            int units;
+            ;
+            units = 2;
+            while (units <= 128)
             {
-                string[] months = new string[12];
-                months[0] = "January";
-                months[1] = "February";
-                months[2] = "March";
-                months[3] = "April";
-                months[4] = "May";
-                months[5] = "June";
-                months[6] = "July";
-                months[7] = "August";
-                months[8] = "September";
-                months[9] = "October";
-                months[10] = "November";
-                months[11] = "December";
-
-                for (int i = 0; i < 12; i++)
                 {
-                    Console.WriteLine(i + " " + months[i]);
-                }
-                {
-                    string[] seasons = new string[4];
-                    seasons[0] = "Spring";
-                    seasons[1] = "Summer";
-                    seasons[2] = "Autumn";
-                    seasons[3] = "Winter";
-                    foreach (string season in seasons)
-                    { Console.WriteLine(season); }
+                    Console.Write("[");
+                    Console.Write(units); Console.Write("]"); Console.WriteLine();
+                    units = units * 2;
 
+
+                    for (int l = 49; l > 1; l--)
+                        Console.Write(l + ", ");
+                    for (int l = 1; l == 1; l--)
+                        Console.WriteLine(l);
+
+                    int unitz;
+                    ;
+                    unitz = 1;
+                    while (unit < 21)
                     {
-                        int[] numbers = new int[1000];
-                        Random random = new Random();
-                        for (int i = 0; i < 1000; i++)
                         {
-                            int randomnumber = random.Next(0, 100);
-                            numbers[i] = randomnumber;
-                        }
-                        foreach (int number in numbers)
-                        { Console.WriteLine(number); }
+                            Console.Write(unitz + "   ");
+
+                            unitz = unitz + 2;
+                            if (unitz == 21)
+                                Console.Write(unitz + "\n");
+                                }
+
+                                {
+
+                                    int n = 8;
+                                    int i = 10;
+                                    while (i < n)
+                                    {
+                                        Console.Write("*");
+                                        i++;//Whereas a while loop will test a condition before a code is executed,
+                                            //a do while loop will execute a condition first and evaluate the condition
+                                            //only after its execution.
+
+                                Boolean icyRain;
+                                Boolean tornadoWarning;
+
+                                icyRain = false;
+                                tornadoWarning = false;
 
 
-
-                        {
-                            string[] named = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
-
-                            int i = 0;
-                            while (i < named.Length)
-                            {
-                                Console.WriteLine(named[i]);
-                                i++;
+                                if (icyRain == false && tornadoWarning == false)
+                                    Console.WriteLine("Let's go outside!");
 
                             }
-                        }
-                    }
 
                         }
+
+
+
+
+
+
+
+                            }
+                        
+
+
                 }
+            }
         }
     }
 }
+        
+
+
+
+
+
+
+    
+
+            
+        
+    
+
+
