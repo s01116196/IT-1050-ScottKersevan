1d3570387e4e747c25bd869697f4307dca9b7a0b
s01116196 <33640790+s01116196@users.noreply.github.com>
2017-12-14 04:19:07 -0500

    Add files via upload

---
+++ IT1050-MidtermProgram.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT1050_Midterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int unit = 1;
+            Boolean keepLooping = true;
+            while (keepLooping == true) Console.Write(unit + 1);
+
+            int units;
+            ;
+            units = 2;
+            while (units <= 128)
+            {
+                {
+                    Console.Write("[");
+                    Console.Write(units); Console.Write("]"); Console.WriteLine();
+                    units = units * 2;
+
+
+                    for (int l = 49; l > 1; l--)
+                        Console.Write(l + ", ");
+                    for (int l = 1; l == 1; l--)
+                        Console.WriteLine(l);
+
+                    int unitz;
+                    ;
+                    unitz = 1;
+                    while (unit < 21)
+                    {
+                        {
+                            Console.Write(unitz + "   ");
+
+                            unitz = unitz + 2;
+                            if (unitz == 21)
+                                Console.Write(unitz + "\n");
+                                }
+
+                                {
+
+                                    int n = 8;
+                                    int i = 10;
+                                    while (i < n)
+                                    {
+                                        Console.Write("*");
+                                        i++;//Whereas a while loop will test a condition before a code is executed,
+                                            //a do while loop will execute a condition first and evaluate the condition
+                                            //only after its execution.
+
+                                Boolean icyRain;
+                                Boolean tornadoWarning;
+
+                                icyRain = false;
+                                tornadoWarning = false;
+
+
+                                if (icyRain == false && tornadoWarning == false)
+                                    Console.WriteLine("Let's go outside!");
+
+                            }
+
+                        }
+
+
+
+
+
+
+
+                            }
+                        
+
+
+                }
+            }
+        }
+    }
+}
+        
+
+
+
+
+
+
+    
+
+            
+        
+    
+
+
---
+++ Lab09Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Question 1:");
+            Console.WriteLine("");
+            var fruits = new[] { "apple", "mango", "orange", "apricot", "cherry", "grape", "blueberry" };
+
+            foreach (var fruit in fruits)
+            { Console.WriteLine(fruit); }
+            Console.WriteLine("");
+            Console.WriteLine("Question 2:");
+            IEnumerable<string> query = from fruit in fruits
+                                        where fruit[0].Equals('a')
+
+                                        select fruit.ToUpper();
+            foreach (var fruit in query)
+Console.WriteLine(fruit);
+            Console.WriteLine("");
+            {
+
+                
+                Console.WriteLine("Questions 3-4:");
+
+                List<DateTime> months = new List<DateTime> {new DateTime (2017, 7, 4), new DateTime(2017, 8, 4), new DateTime(2017, 9, 4),
+                new DateTime(2017, 10, 4), new DateTime(2017, 11, 4),
+                new DateTime(2017, 12, 4) };
+                Console.WriteLine(months.Count);
+                Console.WriteLine(months.Capacity);
+                Console.WriteLine("");
+
+                foreach (DateTime month in months)
+                { Console.WriteLine(month.ToString("MMMM")); }
+                Console.WriteLine("");
+                months.AddRange(new List<DateTime> {new DateTime (2017, 1, 4), new DateTime(2017, 2, 4), new DateTime(2017, 3, 4),
+                new DateTime(2017, 4, 4), new DateTime(2017, 5, 4),
+                new DateTime(2017, 6, 4) });
+                months.Sort();
+                foreach (DateTime month in months)
+                { Console.WriteLine(month.ToString("MMMM")); }
+                Console.WriteLine("");
+
+
+                months.Remove(new DateTime(2017, 1, 4));
+                months.Remove(new DateTime(2017, 2, 4));
+                months.Remove(new DateTime(2017, 3, 4));
+                months.Remove(new DateTime(2017, 4, 4));
+                months.Remove(new DateTime(2017, 5, 4));
+                months.Remove(new DateTime(2017, 6, 4));
+                Console.WriteLine(""); Console.WriteLine(months.Count);
+                Console.WriteLine(months.Capacity);
+                Console.WriteLine();
+
+
+                IEnumerable<DateTime> ber = from month in months
+                                            where month.ToString("MMMM").Contains("ber")
+                                            select month;
+                foreach (DateTime month in ber)
+                { Console.WriteLine(month.ToString("MMMM")); }
+
+
+
+                
+
+            }
+        }
+    }
+}
